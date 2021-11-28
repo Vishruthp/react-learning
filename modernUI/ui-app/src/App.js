7d3f2d62470db095f4992822638e6428ac41ec39
Vishruth <20371189+Vishruthp@users.noreply.github.com>
2021-11-28 18:57:49 +0530

    initial changes

---
+++ modernUI/ui-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Article,Brand,CTA } from './components/fetchComponents'
+const App = () => {
+    return (
+        <div>
+            yello
+        </div>
+    )
+}
+
+export default App
